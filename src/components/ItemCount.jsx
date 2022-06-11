839d528b42545746dbdf3c7edf84d67de5bc19d7
Laura Romero <lauriitaromero_18@hotmail.com>
2022-06-10 22:39:34 -0300

    modificacion en la traduccion de las funciones del itemCount

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -4,27 +4,28 @@ import '../App.css';
 const ItemCount = ({inicial, stock, onAdd}) => {
     const [count, setCount] = useState(inicial)
     
-    const sumar = ()=>{
+    const add = ()=>{
         count < stock ? setCount(count + 1)
         : alert("No hay mas stock")
     }
 
-    const restar = ()=>{
+    const subtract = ()=>{
         count > inicial ? setCount(count - 1)
         : alert ("No se puede quitar mas productos")
     }
 
-    const reset = () =>{
-        setCount(inicial)
-    }
+    const updateCart = () => {
+        onAdd ();
+        setCount(inicial) ;
+    };
 
   return (
     <>
     <div className='container-sm bg-dark text-center m-5' style={{ width: '200px' }}>
         <h2 className='h2Count'>{count}</h2>
-        <button className='btnCount' onClick={sumar}>+</button>
-        <button className='btnCount' onClick={restar}>-</button>
-        <button className='btnCount' onClick={() => {onAdd(count); reset()}}>Agregar al carrito</button>
+        <button className='btnCount'  onClick={add}>+</button>
+        <button className='btnCount'  onClick={subtract}>-</button>
+        <button className='btnCount'  onClick={updateCart}>Agregar al carrito</button>
     </div>
    
     </>
