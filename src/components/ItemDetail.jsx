d2648c9a395adfb7c3eeb332a5e7ac23ac4d3a51
Laura Romero <lauriitaromero_18@hotmail.com>
2022-06-15 23:57:57 -0300

    pre entrega

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -5,18 +5,17 @@ function ItemDetail({detail}) {
     const{title, description, pictureUrl, price, stock} = detail;
   return (
     <>
-      <div className="card" style={{width: '18rem'}}>
-      <img src={`../${pictureUrl}`} className="card-img-top"style={{width: 286 , height: 286}}  alt='Imagen' />  
+      <div className="card" style={{width: '25rem'}}>
+      <img src={`../${pictureUrl}`} className="card-img-top"style={{width: 398 , height: 446}}  alt='Imagen' />  
       <div className="card-body">
          <h5 className="card-title">{title}</h5>
-         <p className="card-text">
-             DESCRIPCION: {description}
-             <br />
-             <br />
-             MONTO: {price}
-             <br />
-             <br />
-             STOCK: {stock}
+         <p className='card-text'>
+          DESCRIPCION: {description} 
+          <br />
+          MONTO: {price} 
+          <br />
+          STOCK: {stock} 
+          <br />
          </p>
        </div>
     </div>
